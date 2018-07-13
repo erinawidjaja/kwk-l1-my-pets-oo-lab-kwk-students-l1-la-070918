8133cc3618e18b78d10d04bb304e3761c3730d41
erinawidjaja <erina.widjaja@gmail.com>
2018-07-13 16:55:03 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
 class Cat
   # code goes here
+  attr_accessor :name, :mood
+  def initialize(name, mood)
+    @name = name 
+    @mood = mood
+  end
 end
+cat_one = Cat.new("Kiwi", "sassy")
+puts cat_one.name
+puts cat_one.mood
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,14 @@
-class Owner
-  # code goes here
+class Owner 
+  attr_accessor :name, :buying, :pets_info
+  def initialize(name, buying, pets_info)
+    @name = name
+    @price = price 
+    @pets_info = pets_info
+  end 
+end 
 
- end
+#   def initialize(species)
+#     @species = species
+#     @pets {:cats => [], :dogs => [], :fishes => []}
+# end 
+# end 
